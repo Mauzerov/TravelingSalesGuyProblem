a03df9f5343c12b4c68a1fc7981a4adadbd648e2
Mauzerov <mauzer.official.1@gmail.com>
2022-11-25 10:17:05 +0100

    tabLayout impl

--- app/src/main/java/com/mauzerov/travelingsalesguyproblem/MainActivity.kt
+++ app/src/main/java/com/mauzerov/travelingsalesguyproblem/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.mauzerov.travelingsalesguyproblem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +14,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.tabLayout.apply {
+            addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    tab?.position?.let {
+                        binding.viewPager.currentItem = it
+                    }
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+
+            })
+        }
         binding.viewPager.apply {
             adapter = TabLayoutAdapter(supportFragmentManager, lifecycle).apply {
-                
+
             }
             orientation = ViewPager2.ORIENTATION_VERTICAL
         }
