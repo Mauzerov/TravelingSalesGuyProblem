64abe5762880ff3852a62a2fa46c05b1d20e293d
Mauzerov <mauzer.official.1@gmail.com>
2022-11-25 10:08:27 +0100

    TaLayoutAdapter

--- app/src/main/java/com/mauzerov/travelingsalesguyproblem/MainActivity.kt
+++ app/src/main/java/com/mauzerov/travelingsalesguyproblem/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mauzerov.travelingsalesguyproblem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.ViewPager2
 import com.mauzerov.travelingsalesguyproblem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewPager.apply {
+            adapter = TabLayoutAdapter(supportFragmentManager, lifecycle).apply {
+                
+            }
+            orientation = ViewPager2.ORIENTATION_VERTICAL
+        }
     }
 }
