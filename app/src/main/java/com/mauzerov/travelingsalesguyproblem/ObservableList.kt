5f520d44c6c0022874632bc9885b3ef79d769573
Mateusz Mazurek <mauzer.official.1@gmail.com>
2022-11-28 18:26:00 +0100

    Custom Observable List Impl

---
+++ app/src/main/java/com/mauzerov/travelingsalesguyproblem/ObservableList.kt
@@ -0,0 +1,28 @@
+package com.mauzerov.travelingsalesguyproblem
+
+import java.util.*
+
+class ObservableList<T>(private val wrapped: MutableList<T>): MutableList<T> by wrapped, Observable() {
+    enum class ChangeType {
+        ADD, REMOVE, SET
+    }
+    data class Argument<T>(val updateType: ChangeType, val element: T)
+
+    override fun add(element: T): Boolean {
+        return if (wrapped.add(element)) {
+            setChanged()
+            notifyObservers(Argument(ChangeType.ADD, element))
+            true
+        }
+        else false
+    }
+
+    override fun remove(element: T): Boolean {
+        return if (wrapped.remove(element)) {
+            setChanged()
+            notifyObservers(Argument(ChangeType.REMOVE, element))
+            true
+        }
+        else false
+    }
+}
