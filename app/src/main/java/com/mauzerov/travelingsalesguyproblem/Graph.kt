3793a0f470f75086b0ed1423c489509fcc479fb4
Mateusz Mazurek <mauzer.official.1@gmail.com>
2022-11-28 19:06:35 +0100

    Graph to Interface Refactor

--- app/src/main/java/com/mauzerov/travelingsalesguyproblem/Graph.kt
+++ app/src/main/java/com/mauzerov/travelingsalesguyproblem/Graph.kt
@@ -1,4 +1,27 @@
 package com.mauzerov.travelingsalesguyproblem
 
-class Graph {
+interface Graph<T> {
+    data class Edge<T>(
+        val from: T,
+        val to: T,
+        val weight: Int
+    )
+
+    fun addNode(node: T)
+
+    fun removeNode(node: T)
+
+    fun addEdge(from: T, to: T, weight: Int)
+
+    fun addEdge(fromIndex: Int, toIndex: Int, weight: Int)
+
+    fun removeEdge(index: Int)
+
+    fun getNodes(): List<T>
+
+    fun getEdges(): List<Edge<T>>
+
+    fun getWeight(from: T, to: T): Int
+
+    fun getWeight(fromIndex: Int, toIndex: Int): Int
 }
