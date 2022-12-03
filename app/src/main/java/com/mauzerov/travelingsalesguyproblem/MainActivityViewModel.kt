c590bcc9b14eb148e8bcf314f77fe058cca0dc09
Mauzerov <mauzer.official.1@gmail.com>
2022-12-03 20:31:20 +0100

    Integrate TSP with Layout

--- app/src/main/java/com/mauzerov/travelingsalesguyproblem/MainActivityViewModel.kt
+++ app/src/main/java/com/mauzerov/travelingsalesguyproblem/MainActivityViewModel.kt
@@ -1,10 +1,13 @@
 package com.mauzerov.travelingsalesguyproblem
 
 import androidx.databinding.BaseObservable
+import androidx.databinding.ObservableArrayList
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.mauzerov.travelingsalesguyproblem.algorithms.travelingSalesMan
 import com.mauzerov.travelingsalesguyproblem.graph.AutoGeneratingGraph
 import com.mauzerov.travelingsalesguyproblem.graph.Graph
+import com.mauzerov.travelingsalesguyproblem.util.ObservableList
 
 class MainActivityViewModel : BaseObservable() {
     // Live Data List Of City Names
@@ -23,6 +26,15 @@ class MainActivityViewModel : BaseObservable() {
     val graph: Graph<String>
         get() = graphObservable.value!!
 
+    var bestPath : ObservableList<String> = ObservableList(mutableListOf())
+    var distance: Int? = null
+
+    fun findBestPath() {
+        val result = graph.travelingSalesMan()
+        bestPath.clear()
+        bestPath.addAll(result.path)
+        distance = result.distance
+    }
     init {
         graph.addNode("ABC")
         graph.addNode("ADC")
