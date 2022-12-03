1546cff0361b4bb0857a8d023c16dc6c711af2ab
Mateusz Mazurek <mauzer.official.1@gmail.com>
2022-12-03 21:42:15 +0100

    Implement Random City Name Api Interface

---
+++ app/src/main/java/com/mauzerov/travelingsalesguyproblem/RandomCityName.kt
@@ -0,0 +1,23 @@
+package com.mauzerov.travelingsalesguyproblem
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
+import java.net.URL
+
+object RandomCityName {
+    operator fun invoke(): Flow<String> = flow<String> {
+        URL("https://random-data-api.com/api/v2/addresses").openConnection()
+            .apply { connect() }.let {
+                val result = it.getInputStream().bufferedReader().readText()
+                // Parse To Json and return city
+                emit(
+                    Json.parseToJsonElement(result)
+                        .jsonObject["city"]!!.toString()
+                )
+            }
+    }.flowOn(Dispatchers.IO)
+}
