e9ee976c8d261cad905be2771410498e6501c714
Mauzerov <mauzer.official.1@gmail.com>
2022-12-03 20:01:34 +0100

    Remove Deprecated Import

--- app/src/main/java/com/mauzerov/travelingsalesguyproblem/adapter/TabLayoutAdapter.kt
+++ app/src/main/java/com/mauzerov/travelingsalesguyproblem/adapter/TabLayoutAdapter.kt
@@ -2,7 +2,6 @@ package com.mauzerov.travelingsalesguyproblem.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
