c4167813637e3962fb9db9be5946f9f34699c5e9
Mateusz Mazurek <mauzer.official.1@gmail.com>
2023-01-03 23:12:56 +0100

    Update Combinations.kt
    Less readable but more preferred

--- app/src/main/java/com/mauzerov/travelingsalesguyproblem/algorithms/Combinations.kt
+++ app/src/main/java/com/mauzerov/travelingsalesguyproblem/algorithms/Combinations.kt
@@ -11,7 +11,7 @@ fun <T>Iterable<T>.combinations(r: Int) : Sequence<List<T>> = sequence {
         yield(indices.map { pool[it] })
         var i = r
         do {
-            i--; if (i == -1) return@sequence
+            if (--i == -1) return@sequence
         } while (indices[i] == i + n - r )
         indices[i]++
         for (j in i+1 until r)
